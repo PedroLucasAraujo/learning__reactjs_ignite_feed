2d6c4d083cfb6e020912b2273094425086857b4c
PedroLucasAraujo <pedrolucas.araujo@agenciam3.com>
2024-08-28 20:57:09 -0300

    feat: Estilizações de Comentários

---
+++ src/components/Comment.jsx
@@ -0,0 +1,36 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img
+        src="https://github.com/PedroLucasAraujo.png"
+        alt="Pedro Lucas Araujo"
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Pedro Lucas Araujo</strong>
+              <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Deletar Comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!!</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,5 @@
 // props: { author, content}
+import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 export function Post(props) {
   return (
@@ -45,6 +46,12 @@ export function Post(props) {
           <button type="submit">Publicar</button>
         </footer>
       </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
   );
 }

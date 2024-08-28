369bf92ff3ea486c536b9e019a80d12de5b3b8fa
PedroLucasAraujo <pedrolucas.araujo@agenciam3.com>
2024-08-28 18:20:44 -0300

    class05: Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import { Post } from "./Post";
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Diego Fernandes"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque error quisquam, sit voluptatem obcaecati accusantium eaque deserunt, perspiciatis ipsum, necessitatibus quibusdam accusamus impedit asperiores eius soluta voluptatibus aspernatur? Expedita, culpa?"
+      />
+      <Post author="Pedro Lucas" content="Um Novo post muito legal!" />
     </div>
   );
 }

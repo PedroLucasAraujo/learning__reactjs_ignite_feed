a2a672ac1fa6abde208206c5f4f5562333767836
PedroLucasAraujo <pedrolucas.araujo@agenciam3.com>
2024-08-28 18:57:04 -0300

    class08: componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import styles from "./Header.module.css";
-
+import igniteLogo from "../assets/igniteLogo.svg";
 export function Header() {
   return (
     <header className={styles.header}>
+      <img src={igniteLogo} alt="Ignite Feed" />
       <strong>Ignite Feed</strong>
     </header>
   );
